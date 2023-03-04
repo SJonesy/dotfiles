456a0034ff6288b24966d8adcf5ffaf60c7efcb6
steve <stephen@jonesing.net>
2023-03-03 22:07:02 -0600

    add bash and setup.sh

---
+++ .config/bash_additions.sh
@@ -0,0 +1,3 @@
+alias ll="ls -lah"
+alias l="ls -a"
+alias config='/usr/bin/git --git-dir=/home/anon/.config/ --work-tree=/home/anon'
---
+++ .config/setup.sh
@@ -0,0 +1,4 @@
+echo "" >> $HOME/.bashrc
+echo "# Added by setup.sh" >> $HOME/.bashrc
+echo "source \$HOME/.config/bash_additions.sh" >> $HOME/.bashrc
+
